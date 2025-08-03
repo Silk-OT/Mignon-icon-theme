00b2ca779c4c0c66ba555756665ca1aed72ec115
Igor Moraes <igorfm13524@gmail.com>
2025-08-03 12:44:04 -0300

    🧹 Remove and clean symbolic icons
    Revert icons used in the panel and inside Nautilus back to GNOME defaults

--- install.sh
+++ install.sh
@@ -18,25 +18,16 @@ install_theme() {
 	# Update the name in index.theme
 	sed -i "s/%NAME%/${THEME_NAME//-/ }/g" "${THEME_DIR}/index.theme"
 
-	cp -r src/{16,22,24,32,symbolic} "${THEME_DIR}"
 	mkdir -p "${THEME_DIR}/scalable"
 	cp -r src/scalable/{apps,devices,mimetypes} "${THEME_DIR}/scalable"
 	cp -r src/scalable/places "${THEME_DIR}/scalable/places"
 
-	sed -i "s/#5294e2/${theme_color}/g" "${THEME_DIR}/scalable/apps/"*.svg "${THEME_DIR}/scalable/places/"default-*.svg "${THEME_DIR}/16/places/"folder*.svg
-	sed -i "/\ColorScheme-Highlight/s/currentColor/${theme_color}/" "${THEME_DIR}/scalable/places/"default-*.svg "${THEME_DIR}/16/places/"folder*.svg
+	sed -i "s/#5294e2/${theme_color}/g" "${THEME_DIR}/scalable/apps/"*.svg "${THEME_DIR}/scalable/places/"default-*.svg
+	sed -i "/\ColorScheme-Highlight/s/currentColor/${theme_color}/" "${THEME_DIR}/scalable/places/"default-*.svg
 	sed -i "/\ColorScheme-Background/s/currentColor/#ffffff/" "${THEME_DIR}/scalable/places/"default-*.svg
 
-	cp -r links/{16,22,24,32,scalable,symbolic} "${THEME_DIR}"
-	cp -r links/16/{actions,devices,places} "${THEME_DIR}/16"
-	cp -r links/22/{actions,devices,places} "${THEME_DIR}/22"
-	cp -r links/24/{actions,devices,places} "${THEME_DIR}/24"
-	cp -r links/symbolic/* "${THEME_DIR}/symbolic"
+	cp -r links/scalable "${THEME_DIR}"
 
-	ln -sr "${THEME_DIR}/16"                                                       "${THEME_DIR}/16@2x"
-	ln -sr "${THEME_DIR}/22"                                                       "${THEME_DIR}/22@2x"
-	ln -sr "${THEME_DIR}/24"                                                       "${THEME_DIR}/24@2x"
-	ln -sr "${THEME_DIR}/32"                                                       "${THEME_DIR}/32@2x"
 	ln -sr "${THEME_DIR}/scalable"                                                 "${THEME_DIR}/scalable@2x"
 
 	gtk-update-icon-cache "${THEME_DIR}"
